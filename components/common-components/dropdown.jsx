14b40c059b9d674ce21666b1173d9f20fe9e0c46
nsathiyakala <nsathiyakala6@gmail.com>
2026-03-05 17:45:24 +0530

    custome select style

--- components/common-components/dropdown.jsx
+++ components/common-components/dropdown.jsx
@@ -74,7 +74,7 @@ const CustomSelect = (props) => {
         {selectedOption && !disabled && (
           <button
             onClick={() => onChange(null)}
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+            className="absolute right-[-20px] top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
           >
             <X className="w-4 h-4" />
           </button>
