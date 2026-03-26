5a34bb7132cb4cef3faa6481155f4b7fe8c4cee3
nsathiyakala <nsathiyakala6@gmail.com>
2026-03-26 18:40:54 +0530

    changes

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1364,7 +1364,7 @@ export default function NaukriProfilePage() {
                           <img
                             src={
                               state.userDetail?.profile_logo_url ||
-                              "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
+                              "/assets/images/dummy-profile-pic.png"
                             }
                             alt="Profile"
                             width={128}
